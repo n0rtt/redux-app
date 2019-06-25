a93b4cb9a943af82bfab04805d4e0be5222e2bc4
Miguel Osorio <mial.osorio@gmail.com>
2019-06-25 16:44:51 +0200

    lesson 12

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + 1
+        case 'DEC':
+            return state - 1
+        case 'RST':
+            return 0
+        default:
+            return state
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const inc = () => ({type: 'INC'})
+const dec = () => ({type: 'DEC'})
+const rst = () => ({type: 'RST'})
+
+const store = createStore(reducer)
+
+document.getElementById('plus').addEventListener('click', () => {
+    store.dispatch(inc())
+})
+document.getElementById('minus').addEventListener('click', () => {
+    store.dispatch(dec())
+})
+document.getElementById('reset').addEventListener('click', () => {
+    store.dispatch(rst())
+})
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState()
+}
+
+store.subscribe(update)
